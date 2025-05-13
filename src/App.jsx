5ec0766a8981aea3c1afcee93c4671c2df123340
Abaljerind <abaljerinda@gmail.com>
2025-05-13 10:57:44 +0700

    Invoked these components
    Invoked NavBar, Hero, Features, Downloads, FAQ, Subscribe & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import Downloads from "./components/Downloads";
+import FAQ from "./components/FAQ";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Subscribe from "./components/Subscribe";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-newDarkBlue text-3xl font-bold underline">
-        Hello World
-      </h1>
-    </div>
+    <>
+      <NavBar />
+      <Hero />
+      <Features />
+      <Downloads />
+      <FAQ />
+      <Subscribe />
+      <Footer />
+    </>
   );
 }
 
