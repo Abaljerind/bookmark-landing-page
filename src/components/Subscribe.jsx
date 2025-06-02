8e2a08c4bd161735536caad8ae8c50e8c2b02066
Abaljerind <abaljerinda@gmail.com>
2025-06-02 14:18:38 +0700

    Applied styles for md breakpoint
    Applied styles to: p, form, input, button and br
    Added br tag inside h2 tag
    Added autoComplete with value off

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,26 +3,28 @@ const Subscribe = () => {
     <section className="bg-newBlue py-16">
       {/* header */}
       <header className="px-6 text-center">
-        <p className="text-newGrey mb-4 text-sm font-medium tracking-[0.25em] uppercase">
+        <p className="text-newGrey mb-4 text-sm font-medium tracking-[0.25em] uppercase md:mb-6">
           35,000+ already joined
         </p>
         <h2 className="text-newGrey text-2xl font-medium">
-          Stay up-to-date with what we’re doing
+          Stay up-to-date with what <br className="hidden md:block" /> we’re
+          doing
         </h2>
       </header>
       {/* ./ header */}
 
       {/* input form */}
-      <form className="mt-6 px-6 text-center">
+      <form className="mt-6 px-6 text-center md:flex md:items-center md:justify-center md:gap-3">
         <input
           type="email"
           id="email"
-          className="bg-newGrey mb-4 w-full rounded-md p-3 text-sm placeholder:text-gray-400 focus:outline-0"
+          className="bg-newGrey mb-4 w-full rounded-md p-3 text-sm placeholder:text-gray-400 focus:outline-0 md:mb-0 md:w-60"
           placeholder="Enter your email address"
+          autoComplete="off"
         />
         <button
           type="submit"
-          className="bg-newRed text-newGrey w-full cursor-pointer rounded-md px-4 py-3 text-sm font-medium shadow-lg"
+          className="bg-newRed text-newGrey w-full cursor-pointer rounded-md px-4 py-3 text-sm font-medium shadow-lg md:w-28"
         >
           Contact Us
         </button>
