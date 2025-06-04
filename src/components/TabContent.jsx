f942d124b992c741553f97d80e584d085ea0a99a
Abaljerind <abaljerinda@gmail.com>
2025-06-04 15:18:46 +0700

    Applied styles for md & lg breakpoint
    Applied styles for lg breakpoint to: 2 <div>, <img>, <p> and <button>
    Fixed styles for <img> in mobile device width
    Applied styles for md breakpoint to: <p> and <button>

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -3,24 +3,26 @@ const TabContent = ({ content }) => {
     <>
       {/* Content image */}
       <div className="relative mb-[74px] md:mb-0 md:w-1/2">
-        <div className="bg-newBlue absolute -bottom-8 -left-2 -z-[1] h-5/6 w-10/12 rounded-r-full md:-bottom-12 md:-left-4"></div>
+        <div className="bg-newBlue absolute -bottom-8 -left-2 -z-[1] h-5/6 w-10/12 rounded-r-full md:-bottom-12 md:-left-4 lg:-left-14 lg:h-60 lg:w-11/12"></div>
         <img
           src={content.image}
           alt={content.alt}
-          className="mx-auto size-72 h-auto"
+          className="mx-auto mr-12 h-48 w-64 md:mr-0 md:h-52 md:w-72 lg:mr-8 lg:h-64 lg:w-96"
         />
       </div>
       {/* ./ Content image */}
 
       {/* Content text */}
-      <div className="px-6 text-center md:w-1/2 md:text-start">
+      <div className="px-6 text-center md:w-1/2 md:text-start lg:w-2/5 lg:pr-16">
         <h3 className="text-newDarkBlue mb-4 text-2xl font-semibold">
           {content.title}
         </h3>
-        <p className="text-newDarkBlue/50 mb-4">{content.text}</p>
+        <p className="text-newDarkBlue/50 mb-4 md:text-sm lg:text-base">
+          {content.text}
+        </p>
         <button
           type="button"
-          className="bg-newBlue text-newGrey cursor-pointer rounded-md px-6 py-3 font-medium shadow-lg"
+          className="bg-newBlue text-newGrey cursor-pointer rounded-md px-6 py-3 font-medium shadow-lg md:text-sm lg:text-base"
         >
           More Info
         </button>
