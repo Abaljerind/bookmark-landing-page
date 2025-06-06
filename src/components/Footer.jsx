c289d0737ac1cf340935ac41f426f1ff37649167
Abaljerind <abaljerinda@gmail.com>
2025-06-06 10:59:20 +0700

    Style(Footer): adjust styles for lg breakpoint
    Adjust inline padding for <footer>
    Adjust width for <img>
    Adjust font size for <p>
    Adjust size for 2 <img> of media social

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,15 @@ const Footer = () => {
   const navItems = ["features", "pricing", "contact"];
 
   return (
-    <footer className="bg-newDarkBlue flex flex-col items-center py-10 md:flex-row md:justify-between md:px-6">
+    <footer className="bg-newDarkBlue flex flex-col items-center py-10 md:flex-row md:justify-between md:px-6 lg:px-16">
       {/* logo + nav */}
       <div className="mb-12 flex flex-col items-center gap-8 md:mb-0 md:flex-row">
         {/* logo */}
-        <img src="./images/logo-bookmark-footer.svg" alt="logo bookmark" />
+        <img
+          src="./images/logo-bookmark-footer.svg"
+          alt="logo bookmark"
+          className="lg:w-44"
+        />
         {/* ./ logo */}
 
         {/* nav item */}
@@ -15,7 +19,7 @@ const Footer = () => {
             return (
               <p
                 key={index + 1}
-                className="text-newGrey mb-5 cursor-pointer text-center tracking-widest uppercase last:mb-0 md:mb-0"
+                className="text-newGrey mb-5 cursor-pointer text-center tracking-widest uppercase last:mb-0 md:mb-0 lg:text-lg"
               >
                 <a href="#">{nav}</a>
               </p>
@@ -32,14 +36,14 @@ const Footer = () => {
           <img
             src="./images/icon-facebook.svg"
             alt="Icon Facebook"
-            className="size-6 cursor-pointer"
+            className="size-6 cursor-pointer lg:size-7"
           />
         </a>
         <a href="https://www.twitter.com">
           <img
             src="./images/icon-twitter.svg"
             alt="Icon Twitter"
-            className="size-6 cursor-pointer"
+            className="size-6 cursor-pointer lg:size-7"
           />
         </a>
       </div>
