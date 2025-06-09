35760ccc38e60d40cc8e8586d244d22102726300
Abaljerind <abaljerinda@gmail.com>
2025-06-09 11:37:20 +0700

    Feat(Subscribe): add email validation with error UI
    Add useState for email and error
    Add validateEmail() and handleSubmit() functions
    Add onChange and onBlur listeners on <input> for real-time validation
    Show error icon and message on invalid email input
    Style adjustments for input wrapper, icon, and error message

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+
 const Subscribe = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  const validateEmail = (email) => {
+    const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    if (email.trim() === "") {
+      setError(false);
+      return;
+    }
+
+    setError(!isValid);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (error) return;
+
+    setTimeout(() => {
+      setEmail("");
+    }, 350);
+  }
+
   return (
     <section className="bg-newBlue py-16">
       {/* header */}
@@ -15,17 +39,38 @@ const Subscribe = () => {
 
       {/* input form */}
       <form className="mt-6 px-6 text-center md:flex md:items-center md:justify-center md:gap-3">
-        <input
-          type="email"
-          id="email"
-          className="bg-newGrey mb-4 w-full rounded-md p-3 text-sm placeholder:text-gray-400 focus:outline-0 md:mb-0 md:w-60 lg:w-72 lg:text-base"
-          placeholder="Enter your email address"
-          autoComplete="off"
-        />
+        <div
+          className={`relative rounded-md ${error && "bg-newRed top-8 -mt-4 mb-12 p-0.5 pb-4 md:-mt-8 md:mb-0 md:pb-8"}`}
+        >
+          <input
+            type="email"
+            id="email"
+            className="bg-newGrey mb-4 w-full rounded-md p-3 text-sm placeholder:text-gray-400 focus:outline-0 md:mb-0 md:w-60 lg:w-72 lg:text-base"
+            placeholder="Enter your email address"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={() => validateEmail(email)}
+          />
+          {error && (
+            <img
+              src="/images/icon-error.svg"
+              alt="icon error"
+              className="absolute top-4 right-3"
+            />
+          )}
+          {error && (
+            <p
+              className={`absolute top-12 mt-1 pl-3 text-start text-sm text-white italic lg:mt-2`}
+            >
+              Whoops, make sure it's an email
+            </p>
+          )}
+        </div>
         <button
           type="submit"
           className="bg-newRed text-newGrey hover:text-newRed hover:ring-newRed w-full cursor-pointer rounded-md px-4 py-3 text-sm font-medium shadow-lg transition-all duration-300 hover:bg-white hover:ring md:w-28 lg:w-32 lg:text-base"
-          onClick={(e) => e.preventDefault()}
+          onClick={(e) => handleSubmit(e)}
         >
           Contact Us
         </button>
